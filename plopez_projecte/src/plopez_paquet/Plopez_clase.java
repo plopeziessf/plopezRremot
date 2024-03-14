389fba551f6825a678f80ece293a8976a4c4325e
administrador <administrador@ubuntuvm>
2024-03-14 10:35:56 +0100

    "Examen d'Entorns de Desenvolupament"

--- plopez_projecte/src/plopez_paquet/Plopez_clase.java
+++ plopez_projecte/src/plopez_paquet/Plopez_clase.java
@@ -3,8 +3,7 @@ package plopez_paquet;
 public class Plopez_clase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Examen d'Entorns de Desenvolupament.");
 	}
 
 }
